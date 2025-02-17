d1783bd4ff4c0864c21ab764a5ddcc76e555c720
Nguyen Minh Quang <quangkedo@gmail.com>
2025-02-17 10:25:36 +0700

    Initial commit

---
+++ src/main/java/cviettel/sourcetraining/SourceTrainingApplication.java
@@ -0,0 +1,13 @@
+package cviettel.sourcetraining;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SourceTrainingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SourceTrainingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/cviettel/sourcetraining/SourceTrainingApplicationTests.java
@@ -0,0 +1,13 @@
+package cviettel.sourcetraining;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SourceTrainingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
